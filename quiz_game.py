a072bfb235a3304fb732d8559ebec57cdc5327ee
Yasmine <yasmine@Yasmines-Air.lan>
2025-04-10 16:41:34 +0200

    added questions

--- quiz_game.py
+++ quiz_game.py
@@ -9,7 +9,19 @@ if playing != "yes":
 print("Great! Let's start the quiz.")
 score = 0
 
+Questions = {
+    "What is the capital of France?": "Paris",
+    "What is 2 + 2?": "4",
+    "Who wrote 'Romeo and Juliet'?": "Shakespeare",
+    "What is the chemical symbol for gold?": "Au"
+}
 
+for question, answer in Questions.items():
+    user_answer = input(question + " ")
+    if user_answer.lower() == answer.lower():
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect! The correct answer is " + answer)
 
-
-print("You got" + str(score) + " questions correct!")
+print("You got " + str(score) + " questions correct!")
